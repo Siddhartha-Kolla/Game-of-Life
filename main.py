993f4f429cf7ed7835aa875f09c1a21b95c1138d
Siddhartha Kolla <siddharthansg@gmail.com>
2023-01-24 12:34:30 +0100

    Added Check neighbours function

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
 import numpy as np
 import pygame as pygame
+import random
 
 # Initializing pygame
 pygame.init()
 
 # Number of rows and columns
-rows , cols = 50,50
+rows , cols = 10,10
 
 # Creating a numpy array to check whether cell is alive or not
 cell_status_list = np.zeros((rows,cols),np.int8)
-print(cell_status_list)
+# cell_status_list = [[random.getrandbits(1) for i in range(10)],[random.getrandbits(1) for i in range(10)],[random.getrandbits(1) for i in range(10)],[random.getrandbits(1) for i in range(10)],[random.getrandbits(1) for i in range(10)],[random.getrandbits(1) for i in range(10)],[random.getrandbits(1) for i in range(10)],[random.getrandbits(1) for i in range(10)],[random.getrandbits(1) for i in range(10)],[random.getrandbits(1) for i in range(10)]]
 
 # Creating a numpy array for the cell status after checking the neighbors
 future_status_list = np.zeros((rows,cols),np.int8)
-print(future_status_list)
 
 # Size of each cell
 cell_size = 15
@@ -44,6 +44,19 @@ pygame.display.set_icon(logo)
 
 # Game Loop
 running = True
+
+def check_neighbours():
+    for i in range(rows):
+        for j in range(cols):
+            neib_temp = 0
+            for x in range(-1,2):
+                for y in range(-1,2):
+                    if x != 0 and y != 0:
+                        if not (x+i < 0 or y+j < 0 or x+i >= rows or y+j >= cols):
+                            if cell_status_list[j+y][x+i] == 1:
+                                neib_temp = neib_temp + 1
+            print(neib_temp)
+            future_status_list[i][j] = neib_temp
 while running:
 
     # Checking events
@@ -66,10 +79,13 @@ while running:
     
     for y in range(rows):
         for x in range(cols):
-            pygame.draw.rect(screen,cell_d_color if cell_status_list[y , x] == 0 else cell_a_color,[x*(b_wid+cell_size),y*(b_wid+cell_size),cell_size,cell_size])
+            pygame.draw.rect(screen,cell_d_color if cell_status_list[y][x] == 0 else cell_a_color,[x*(b_wid+cell_size),y*(b_wid+cell_size),cell_size,cell_size])
     
     # Updating(Flipping) the whole screen
     pygame.display.flip()
-# cell_status_list[MouseY/(cell_size+b_wid) , MouseX/(cell_size+b_wid)]
 # Exit the program
+
+check_neighbours()
+print(future_status_list[0])
+
 pygame.quit()
